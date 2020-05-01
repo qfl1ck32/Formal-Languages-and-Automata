093bd64087941d19667a9847c37b91d770c03bc8
qfl1ck32 <56713436+qfl1ck32@users.noreply.github.com>
2020-05-01 07:51:51 +0300

    Update Deterministic Finite Automata Acceptor.py

--- Homework 1/Deterministic Finite Automata Acceptor.py
+++ Homework 1/Deterministic Finite Automata Acceptor.py
@@ -1,6 +1,6 @@
 from graphviz import Digraph
 
-class DeterministicFiniteAutomata:
+class DFA_Acceptor:
 
     def __init__(self, nume):
 
@@ -9,13 +9,13 @@ class DeterministicFiniteAutomata:
         self.states = f.readline().split()
         self.initial_state = f.readline().strip()
         self.actual_state = self.initial_state
-        self.states_finale = f.readline().split()
-        aux = [x for x in f.readline().split(', ')]
-        self.DeterministicFiniteAutomata = []
+        self.final_states = f.readline().split()
+        self.data = []
         self.solution = []
 
-        for elem in aux:
-            self.DeterministicFiniteAutomata.append((elem[0], elem[2], elem[4]))
+        for transition in [x for x in f.readline().split(', ')]:
+            self.data.append((transition[0], transition[2], transition[4]))
+
         self.word = f.readline()
 
     def check_acceptance(self):
@@ -34,7 +34,7 @@ class DeterministicFiniteAutomata:
                 print("λ ∈ DFA.")
             else:
                 for character in self.word:
-                    transition = [x for x in self.DeterministicFiniteAutomata if x[0] == self.actual_state and x[1] == character]
+                    transition = [x for x in self.data if x[0] == self.actual_state and x[1] == character]
                     if transition == []:
                         ok = 0
                         break
@@ -42,38 +42,41 @@ class DeterministicFiniteAutomata:
                         self.actual_state = transition[0][2]
                         self.solution.append(*transition)
 
-                if self.actual_state in self.states_finale and ok == 1:
+                if self.actual_state in self.final_states and ok == 1:
                     print("%s ∈ DFA." % self.word)
                 else:
                     print("%s ∉ DFA, as the word ends in a non-final state." % self.word)
                     self.solution = []
 
-    def draw(self, culoare_graf, culoare_sol):
-        g = Digraph('G', filename = 'DeterministicFiniteAutomata', format = 'png')
-        g.attr('node', shape = 'doublecircle')
+    def draw(self, graph_color, solution_color):
+        graph = Digraph('G', filename = 'Deterministic Finite Automata Acceptor', format = 'png')
+        graph.attr('node', shape = 'doublecircle')
 
-        for current_node in self.states_finale:
-            g.node('%s' % current_node)
+        for current_node in self.final_states:
+            graph.node('%s' % current_node)
 
-        g.attr('node', shape = 'circle')
-        g.node('%s' % self.initial_state)
+        graph.attr('node', shape = 'circle')
+        graph.node('%s' % self.initial_state)
 
-        g.attr('node', shape = 'none')
-        g.edge('', '%s' % self.initial_state, label = 'Start')
+        graph.attr('node', shape = 'none')
+        graph.edge('', '%s' % self.initial_state, label = 'Start')
 
-        g.attr('node', shape = 'circle')
+        graph.attr('node', shape = 'circle')
 
-        for transition in self.DeterministicFiniteAutomata:
+        for transition in self.data:
             if transition in self.solution:
-                g.attr('edge', color = culoare_sol)
-                g.edge('%s' % elem[0], '%s' % elem[2], label = '%s' % elem[1])
-                g.attr('edge', color = culoare_graf)
+                graph.attr('edge', color = solution_color)
+                graph.edge('%s' % transition[0], '%s' % transition[2], label = '%s' % transition[1])
+                graph.attr('edge', color = graph_color)
             else:
-                g.edge('%s' % elem[0], '%s' % elem[2], label = '%s' % elem[1])
-        g.view()
+                graph.edge('%s' % transition[0], '%s' % transition[2], label = '%s' % transition[1])
+        graph.view()
+
+def main():
+    a = DFA_Acceptor('data.in')
+    a.check_acceptance()
+    a.draw('red', 'blue')
 
 
 if __name__ == "__main__":
-    a = DeterministicFiniteAutomata('data.in')
-    a.check_acceptance()
-    a.draw('red', 'blue')
+    main()
